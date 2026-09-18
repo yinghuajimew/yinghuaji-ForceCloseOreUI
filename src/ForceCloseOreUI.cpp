354df9b043617e9c128b5beb5060d9739fd0f3af
樱花祭 <143211469+yinghuajimew@users.noreply.github.com>
2026-09-18 21:09:30 +0800

    Add files via upload

--- src/ForceCloseOreUI.cpp
+++ src/ForceCloseOreUI.cpp
@@ -257,7 +257,7 @@ public:
 #elif __aarch64__
 #define OREUI_PATTERN                                                                     \
      std::initializer_list<const char *>({                                                \
-      "? ? ? D1 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? 91 ? ? ? D5 FA 03 03 2A F7 03 02 2A ? ? ? F9 F4 03 01 AA", \
+      "? ? ? D1 ? ? ? A9 ? ? ? F9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? 91 ? ? ? D5 F5 03 03 2A F6 03 02 2A ? ? ? F9 F4 03 01 AA", \
   })                                                                                                                    \
 
 #endif
